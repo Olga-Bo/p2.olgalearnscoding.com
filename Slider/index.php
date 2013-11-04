d47deb1ea2fdb19cf8ddd45e275fa5dcbfc4dc6e
Olga-Bo <bolkunova.o@gmail.com>
2013-11-03 21:45:53 -0500

    deleted slider

--- Slider/index.php
+++
@@ -1,8 +0,0 @@
-<div id="feature_slider">  
-    <ul>  
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/htdocs/Slider/images/children-playing-outside.jpg" alt="First Slide" /></a></li>  
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/image2.jpg" alt="Second Slide" /></a></li>  
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/image3.jpg" alt="Third Slide" /></a></li>  
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/image4.jpg" alt="Fourth Slide" /></a></li>  
-    </ul>  
-</div>  
