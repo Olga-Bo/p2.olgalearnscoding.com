25938430d804cd2c723d2f10b8d61ee741c4ca7d
Olga-Bo <bolkunova.o@gmail.com>
2013-11-05 12:49:27 -0500

    Sign up page validation

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,26 +1,3 @@
-	<script type="text/javascript">
-		
-	$(function() {
-		// highlight 
-		var elements = $("input[type!='submit'], textarea, select");
-		elements.focus(function(){
-			$(this).parents('li').addClass('highlight');
-		});
-		elements.blur(function(){
-			$(this).parents('li').removeClass('highlight');
-		});
-		
-		$("#forgotpassword").click(function() {
-			$("#password").removeClass("required");
-			$("#login").submit();
-			$("#password").addClass("required");
-			return false;
-		});
-		
-		$("#login").validate()
-	});
-	</script>
-
 
 	<div class="well">
 			<h2>Log in</h2>
--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,20 +1,34 @@
 
-		
-
 
 <div class='well'>
 	<h2>Sign up</h2>
 
-	<form method='POST' action='/users/p_signup' role='form' class='cmxForm' id='login'>
-		<label for="cname">First Name</label><input type='text' name='first_name' class="form-control" id='cname' minlength='2' required />
-		<label>Last Name</label><input type='text' name='last_name' class="form-control">
-		<label for='cmail'>Email</label><input type='text' name='email' class="form-control" id='cmail' required>
-		<label>Password</label><input type='password' name='password' class="form-control">
-	</form>
-	<br>
-	<button type='Submit' class="btn btn-default">Sign up</button>
-
+<form class="cmxform" id="signupForm" method="POST" action='/users/p_signup' role='form'>
+	<fieldset>
+		<div>
+			<label for="first_name">Firstname</label>
+			<input id="first_name" name="first_name" type="text" class='form-control'/>
+		</div>
+		<div>
+			<label for="last_name">Lastname</label>
+			<input id="last_name" name="last_name" type="text" class='form-control'/>
+		</div>
+		<div>
+			<label for="password">Password</label>
+			<input id="password" name="password" type="password" class='form-control'/>
+		</div>
+		<!--<p>
+			<label for="confirm_password">Confirm password</label>
+			<input id="confirm_password" type="password" />
+		</p>-->
+		<div>
+			<label for="email">Email</label>
+			<input id="email" name="email" type="email" class='form-control'/>
+		</div>
+		<br>
+		<button type='Submit' class="btn btn-default">Sign up</button>
+	</fieldset>
+</form>
 </div>
-
 
 				
