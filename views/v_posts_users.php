90c76cb5d13e0d5d6dd96e7a6b80ab8ce54d0ad2
Olga-Bo <bolkunova.o@gmail.com>
2013-11-06 11:46:27 -0500

    posts index thumbnail

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,8 +1,10 @@
 <div class='well'>
 	<h2>Choose users you would like to follow</h2>
+	<div>
 	<?php foreach($users as $user): ?>
 
 		<h3><?=$user['first_name']?> <?=$user['last_name']?></h3>
+		<img id="thumbnail" src='/uploads/avatars/<?=$user['image']?>'>
 		<?php if(isset($connections[$user['user_id']])): ?>
 				<a href='/posts/unfollow/<?=$user['user_id']?>'><button type="button" class="btn btn-warning">Unfollow</button></a>
 		<? else: ?>
@@ -10,4 +12,5 @@
 		<?php endif; ?>
 
 	<?php endforeach ?>
+		<div>
 </div>
--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -3,7 +3,14 @@
 
 
   <form role="form" method='POST' enctype="multipart/form-data" action='/users/profile_update/'>
-                <img class="profile-pic" id="avatar" src="/uploads/avatars/<?= $user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>">                 
+  				<?php if($image): ?>
+                        <img id="avatar" src="/uploads/avatars/<?= $user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>">
+                <?php else: ?>
+                        <img id="avatar" src="/uploads/avatars/default_user.png">
+                <?php endif; ?>
+
+
+                <!--<img id="avatar" src="/uploads/avatars/<?= $user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>">-->                 
                 <div>
                         <label for="avata">Do you want to make some change?</label> 
                         <input type="file" name="avata" id="avata"> 
@@ -12,7 +19,7 @@
                                 Image file must be a jpg, gif, or png.
                                 </div>                
                         <?php endif; ?>
-                        <button type="submit" class="button">Update Image</button>
+                        <button type="submit" id='update-image' class="btn btn-success">Update Image</button>
                 </div>
  </form>
 <?php else: ?>
