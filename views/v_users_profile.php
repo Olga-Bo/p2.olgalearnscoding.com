74233a2da3d3d64a0a36cab8b2629f6d29a7d4c2
Olga-Bo <bolkunova.o@gmail.com>
2013-11-06 10:32:51 -0500

    picture upload function

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,10 +1,20 @@
 <?php if(isset($user)): ?>
 	<h1>Welcome <?=$user->first_name;?> </h1>
 
-	<form action="UploadContent.php" method="POST" enctype="multipart/form-data">
-File:
-<input type="file" name="image"> <input type="submit" value="Upload">
-</form>
+
+  <form role="form" method='POST' enctype="multipart/form-data" action='/users/profile_update/'>
+                <img class="profile-pic" src="/uploads/avatars/<?= $user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>">                 
+                <div>
+                        <label for="avata">Do you want to make some change?</label> 
+                        <input type="file" name="avata" id="avata"> 
+                        <?php if(isset($error)): ?>                   
+                                <div class="messge error">Upload failed.<br> 
+                                Image file must be a jpg, gif, or png.
+                                </div>                
+                        <?php endif; ?>
+                        <button type="submit" class="button">Update Image</button>
+                </div>
+ </form>
 <?php else: ?>
     <h1>No user specified</h1>
 <?php endif; ?>
